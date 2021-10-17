c9b8877c6dc92dd747be2e9afa2e055344b434d5
Raphaël QUEIROZ <raphael_queiroz_1@carrefour.com>
2021-10-17 02:45:25 +0200

    fix memo totalCards

--- src/utils/cards.ts
+++ src/utils/cards.ts
@@ -5,5 +5,5 @@ export type Cards = { [key: number]: number }
 export const totalCards = memoize(
   (cards: Cards, [from, to]) =>
     range(from, to).reduce((p, c) => p + cards[c], 0),
-  (...args) => args.join("_")
+  (cards, ...args) => [Object.values(cards), ...args].join("_")
 )
